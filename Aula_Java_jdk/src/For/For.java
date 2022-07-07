3789dbcb1f9aa5912d6172bee1128f8d43376ec1
Programador Jr <Programador Jr@192.168.0.100>
2022-07-07 00:55:52 -0300

    For e foreach

---
+++ Aula_Java_jdk/src/For/For.java
@@ -0,0 +1,20 @@
+package For;
+
+public class For
+{
+	public static void main(String[] args)
+	{
+		// O fluxo for podemos usar um dentro do outro nas estrutura desejadas.
+		int tamanho = 20;
+		for (int x = 0; x < tamanho; x++)
+		{
+			for (int i = 0; i <= tamanho; i++)//
+			{
+				System.out.print("* ");
+			}
+			System.out.println();// Nessa linha dando o comando para pular para próxima linha 
+		}
+
+	}
+
+}
