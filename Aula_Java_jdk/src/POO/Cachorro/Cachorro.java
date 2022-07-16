4ae146e71e73b926756da858df6a5eb3ccf7020a
terrygomes <leandrogomes.santos>
2022-07-15 23:50:41 -0300

    POO Classe Cachorro

---
+++ Aula_Java_jdk/src/POO/Cachorro/Cachorro.java
@@ -0,0 +1,19 @@
+package POO.Cachorro;
+
+public class Cachorro
+{
+
+	/*
+	 * Nessa classe podemos adicionar todos os atributos de uma classe
+	 * todas as variáveis
+	 */
+
+	int tamanho; // variável tabanho tipo primitivo (Inteiro)
+	String raça; // variável raça tipo String (texto)
+
+	void latir() // Void não renor na valor
+	{
+		System.out.println("Au, au, au");
+	}
+
+}
---
+++ Aula_Java_jdk/src/POO/Cachorro/CachorroTeste.java
@@ -0,0 +1,28 @@
+package POO.Cachorro;
+
+public class CachorroTeste
+{
+
+	public static void main(String[] args)
+	{
+		/*
+		 * Nessa classe criamos 2 objetos sobre 2 cachorros diferentes
+		 * podemos chamar e atriburir as variáveis da classe Cachorro
+		 */
+
+		Cachorro pitbull = new Cachorro(); // Objeto
+		pitbull.raça = "PitBull"; // Atributo valor tipo String Texto
+		pitbull.tamanho = 40; // Atrinuto valor tipo primitivo inteiro
+		pitbull.latir(); // Atrinuto que retorna a ação do método.
+
+		System.out.println(pitbull.raça.toString()); // aqui podemos imprimir qualquer valor e passar para texto.
+
+		Cachorro viralata = new Cachorro();
+		viralata.raça = "ViraLata";
+		viralata.tamanho = 40;
+		viralata.latir();
+
+		System.out.println(viralata.raça.toString());
+	}
+
+}
